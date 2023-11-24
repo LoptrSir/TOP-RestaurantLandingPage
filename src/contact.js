2be648fb820284bfd29c44997083a20703127209
LoptrSir <91218015+LoptrSir@users.noreply.github.com>
2023-11-24 14:22:50 -0700

    info.txt 11/24

---
+++ src/contact.js
@@ -0,0 +1,32 @@
+//Home js
+
+function createContactList() {
+  console.log("create home funct");
+  const contactList = document.createElement("div");
+  contactList.classList.add("contactList");
+  contactList.appendChild(createContact("Phone:", "888.EAT.TACO"));
+  contactList.appendChild(createContact("Website:", "LoptrsTACOS.com"));
+  contactList.appendChild(createContact("Location:", "1234 Taco Drive, SLC, UT 84111"));
+  return contactList;
+}
+
+function createContact (title, description) {
+    const contactItem = document.createElement('div');
+    contactItem.classList.add('contactItem');
+    const contactHeader = document.createElement('h4');
+    // contactHeader.classList.add('contactHeader');
+    contactHeader.textContent = title;
+    const contactData = document.createElement('p');
+    contactData.textContent = description;
+    contactItem.appendChild(contactHeader);
+    contactItem.appendChild(contactData);
+    return contactItem;
+}
+
+function loadContact() {
+  const main = document.querySelector(".main");
+  main.textContent = "";
+  main.appendChild(createContactList());
+}
+console.log("Contact.js");
+export default loadContact;
